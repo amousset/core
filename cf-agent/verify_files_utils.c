037aa6166c2c1eb1cc07d0e499fd22936fcf07f3
Alexis Mousset <alexis.mousset@rudder.io>
2020-10-05 15:03:34 +0200

    Add a keyword to restrict copy_from sources by key

--- cf-agent/verify_files_utils.c
+++ cf-agent/verify_files_utils.c
@@ -3201,7 +3201,7 @@ static AgentConnection *FileCopyConnectionOpen(const EvalContext *ctx,
         else                    /* not found, open and cache new connection */
         {
             int err = 0;
-            conn = ServerConnection(servername, port, conntimeout,
+            conn = ServerConnection(servername, port, EvalContextGetRestrictKeys(ctx), conntimeout,
                                     flags, &err);
 
             /* WARNING: if cache already has non-idle connections to that
@@ -3228,7 +3228,7 @@ static AgentConnection *FileCopyConnectionOpen(const EvalContext *ctx,
     else
     {
         int err = 0;
-        conn = ServerConnection(servername, port, conntimeout,
+        conn = ServerConnection(servername, port, EvalContextGetRestrictKeys(ctx), conntimeout,
                                 flags, &err);
         return conn;
     }
